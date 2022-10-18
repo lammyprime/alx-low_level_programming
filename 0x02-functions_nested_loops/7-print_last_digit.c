5bcdb82518638e9abe5544fefd53ad0de306e8a6
Olamide <lamibanu@gmail.com>
2022-10-18 14:13:35 -0700

    _putchar.c 7-main.c 7-print_last_digit.c -o 7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,12 @@
  */
 int print_last_digit(int n)
 {
-int lastdigit = n % 10;
-return (lastdigit);
+int last;
+last = n % 10;
+if (last < 0)
+{
+last = last * -1;
+ }
+_putchar(last + '0');
+return (last);
 }
