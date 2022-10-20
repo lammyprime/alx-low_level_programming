7adad323aaf2590f153306b0f4390ebbf1e9020a
Olamide <lamibanu@gmail.com>
2022-10-20 09:37:57 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+_putchar(n);
+_putchar('\n');
+if (n <= 0)
+{
+_putchar('\n');
+}
+}
+  
