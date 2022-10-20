4a052631e7f6005383cf8cb14f870b708b540eeb
Olamide <lamibanu@gmail.com>
2022-10-20 06:47:36 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ * @a: to be treated
+ * @b: to be treated
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+multiple = a * b;
+_putchar("%d\n", multiple);
+}
