682f6b6d4c973f7f6402fa2a1b09bf0ccb50ee52
Olamide <lamibanu@gmail.com>
2022-10-20 10:08:01 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n: numver of diagonals
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+int y;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (y = 0; y < n; y++)
+{
+_putchar(92);
+}
+_putchar('\n');
+}
+}
