eeaef5e55998751856f6996007984a80fa6366d6
Olamide <lamibanu@gmail.com>
2022-10-20 09:02:40 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,16 @@
 void more_numbers(void)
 {
 int a, b;
-for (a = '0'; a <= 10; a++)
+for (a = '0'; a < 10; a++)
 {
-for (b = 0; b < 14; b++)
+for (b = '0'; b < 14; b++)
 {
 if (b > 9)
 {
-_putchar(b / 10 + 48);
-_putchar(b % 10 + 48)
+_putchar((b / 10) + 48);
+_putchar((b % 10) + 48);
+}
 }
 _putchar('\n');
-}
 }
 }
