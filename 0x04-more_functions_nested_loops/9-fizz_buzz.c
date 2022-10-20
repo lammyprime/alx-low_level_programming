37cd2c2c076d4d794070cd3d8c850120aa24a065
Olamide <lamibanu@gmail.com>
2022-10-20 13:02:12 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - fizzbizz
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a == 100)
+printf("Buzz");
+else if ((a % 15) == 0)
+{
+printf("FizzBuzz");
+}
+else if ((a % 3) == 0)
+{
+printf("Fizz");
+}
+else if ((a % 5) == 0)
+{
+printf("Buzz");
+}
+else
+printf("%d\t", a);
+}
+return (0);
+}
