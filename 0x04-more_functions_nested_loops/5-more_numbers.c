b3a4666776dadce982a280a1408ae6bcbba033a9
Olamide <lamibanu@gmail.com>
2022-10-20 09:09:53 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 int a, b;
 for (a = '0'; a < 10; a++)
 {
-for (b = '0'; b <= 13; b++)
+for (b = '0'; b <= 14; b++)
 {
 if (b > 9)
 {
-_putchar((b / 10) + 48);
-_putchar((b % 10) + 48);
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
 }
 }
 _putchar('\n');
