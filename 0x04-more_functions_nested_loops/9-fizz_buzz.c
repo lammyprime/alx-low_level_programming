a8517f60605d693c70dc6cf60e63a7ab6a6eeeb9
Olamide <lamibanu@gmail.com>
2022-10-20 13:16:01 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,24 +10,24 @@ int main(void)
 int a;
 for (a = 1; a <= 100; a++)
 {
-if (a == 100)
+if ((a % 15) == 0)
 {
-printf("Buzz");
-}
-else if ((a % 15) == 0)
-{
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if ((a % 3) == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if ((a % 5) == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 printf("%d", a);
+}
+if (x != 100)
+{
+printf(" ");
 }
 printf("\n");
 return (0);
