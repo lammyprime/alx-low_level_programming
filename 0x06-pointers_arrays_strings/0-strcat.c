eb1b0af9dac284c4fcb8c387af7e3bd91f8f0b85
Olamide <lamibanu@gmail.com>
2022-10-26 12:26:33 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int destlen;
-while (dest[destlen])
-destlen++;
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[destlen++] = src[i++];
+  int dlen = 0, i;
+
+  while (dest[dlen])
+    {
+      dlen++;
+    }
+
+  for (i = 0; src[i] != 0; i++)
+    {
+      dest[dlen] = src[i];
+      dlen++;
+    }
+
+  dest[dlen] = '\0';
+  return (dest);
 }
-dest[destlen++] = '\0';
-return (dest);
-}
+
