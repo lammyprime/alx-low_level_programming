140dc9b2fa8960d4d2e9b2920db294746d3cafb6
Olamide <lamibanu@gmail.com>
2022-10-26 14:27:26 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,14 +9,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-for (src[i] = 0; src[i] != '\0'; src[i++])
-{
-dest[i] = src[i++];
-}
-}
-dest[i++] = '\0';
+int i = 0, dest_len = 0;
+while (dest[i++])
+dest_len++;
+for (i = 0; src[i] && i < n; i++)
+dest[dest_len++] = src[i];
 return (dest);
 }
