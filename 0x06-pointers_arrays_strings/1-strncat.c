983f120369f572f6f80189db6c7d7fd51a52ecdd
Olamide <lamibanu@gmail.com>
2022-10-26 07:27:21 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - append a string
+ * @dest: Destination value
+ * @src: Source value
+ * @n: number
+ * Return: the pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+for (src[i] = 0; src[i] != '\0'; src[i++])
+{
+dest[i] = src[i++];
+}
+}
+dest[i++] = '\0';
+return (dest);
+}
