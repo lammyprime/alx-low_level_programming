a711b843e587d6e83ca1f755551be6ffc2793788
Olamide <lamibanu@gmail.com>
2022-10-26 14:52:42 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
-int src_len = 0;
-while (src[i++])
-src_len++;
-for (i = 0; i && src[i] < n; i++)
-{
-dest[i] = src[i];
-}
-for (i = src_len; i < n; i++)
-dest[i] = '\0';
-return (dest);
+  int index = 0, src_len = 0;
+
+  while (src[index++])
+    src_len++;
+
+  for (index = 0; src[index] && index < n; index++)
+    dest[index] = src[index];
+
+  for (index = src_len; index < n; index++)
+    dest[index] = '\0';
+
+  return (dest);
 }
