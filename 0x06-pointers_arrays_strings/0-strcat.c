0a24451ba132d60407c7502187f4a6e26954956c
Olamide <lamibanu@gmail.com>
2022-10-26 06:56:39 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - Copy a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+for (src[i] = 0; src[i] != '\0'; src[i++])
+{
+dest[i] = src[i++];
+}
+}
+dest[i++] = '\0';
+return (dest);
+}
