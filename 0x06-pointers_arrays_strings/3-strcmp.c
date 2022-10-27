cc2ab03c34e9bec68646d44cdae8119f77750b0d
Olamide <lamibanu@gmail.com>
2022-10-26 22:49:08 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,12 @@
 int _strcmp(char *s1, char *s2)
 {
 int i;
- int flag = 0;
- for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+int flag = 0;
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 if (s1[i] != s2[i])
-  {flag = 1;
-    break;
-  }
- if (flag == 1)
+{flag = 1;
+break;
+}
+if (flag == 1)
 return (s1[i] - s2[i]);
 }
