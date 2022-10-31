d575177540003b8b907fffa16f2a75b9d8e69181
Olamide <lamibanu@gmail.com>
2022-10-31 10:14:34 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,9 +9,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+int i;
 char *newsrc = (char *)src;
 char *newdest = (char *)dest;
-for (int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 newdest[i] = newsrc[i];
 }
