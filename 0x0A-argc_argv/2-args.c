677b26e773c9b21343b3c4a850871d1be1a7e9a6
Olamide <lamibanu@gmail.com>
2022-11-04 09:45:01 -0700

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,10 +9,10 @@
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-  int i;
-  for (i = 0; i < argc; i++)
-    {
-      printf("%s\n", argv[i]);
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
 }
   return (0);
 }
