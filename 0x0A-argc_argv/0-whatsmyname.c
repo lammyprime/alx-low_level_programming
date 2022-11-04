6986605b4a27cb968c2788b7fed3dd839cf05891
Olamide <lamibanu@gmail.com>
2022-11-04 08:45:12 -0700

    whatismyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @argv: An array of size argc
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute((__unused__)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
