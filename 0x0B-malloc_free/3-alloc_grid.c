121cf6a191cbe3db2da230f3a5a3d4b89fc42ad0
Olamide <lamibanu@gmail.com>
2022-11-08 03:04:49 -0800

    push

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,20 +18,21 @@ return (NULL);
 grid = malloc(height * sizeof(int *));
 if (grid == NULL)
 {
-free(grid);
 return (NULL);
 }
 for (i = 0; i < height; i++)
 {
 grid[i] = malloc(sizeof(int) * width);
-if (grid == NULL)
+if (grid[i] == NULL)
 {
-free(grid[i]);
+for(j = 0; j < i; j++)
+free(grid[j];
 free(grid);
 return (NULL);
 }
-for (j = 0; j < width; j++)
+for (i = 0; i < height; i++)
 {
+for (j = 0; j < width; j++)  
 grid[i][j] = 0;
 }
 }
