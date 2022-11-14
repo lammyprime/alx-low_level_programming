c539b1e04cb82d994d1d560f2ba0ccb939579702
Olamide <lamibanu@gmail.com>
2022-11-14 09:35:04 -0800

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,12 @@ char *owner;
 
 
 /**
- * dogt - Typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-typedef struct dog dogt;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
