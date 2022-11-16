a23f927a6ab1c7108bb13ca3eaddfc025a77d8c1
Olamide <lamibanu@gmail.com>
2022-11-16 07:20:57 -0800

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,8 +7,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
-if (array != NULL || action != NULL)
-for (i = 0; i < size; i++)
-action(array[i]);
+if (array == NULL || action == NULL)
+return;
+
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 }
