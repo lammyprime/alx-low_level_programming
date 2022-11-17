8f86d1cf6563459bd112f5f46ece2418dddc79f8
Olamide <lamibanu@gmail.com>
2022-11-17 11:21:00 -0800

    numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list numbers;
 unsigned int i;
 va_start(numbers, n);
-if (separator == NULL)
 return;
 for (i = 0; i < n; i++)
 {
